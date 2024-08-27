63d1f817430c26174a6a9cb324f3267a934a6c82
Sean Canning <sean_geo@hotmail.com>
2024-08-27 13:17:34 -0400

    make generic component for moves and evoluation

--- src/components/pokemon-details/evolutions.tsx
+++ src/components/pokemon-details/evolutions.tsx
@@ -1,50 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
-import { capitalizeText } from '../../utils';
-import { ThemeColors } from '../../constants';
+import ListSection from '../list-section';
 
 interface PokemonEvolutionsProps {
   evolutions: Array<{ id: string; name: string }>;
 }
 
 export default function PokemonEvolutions({ evolutions }: PokemonEvolutionsProps) {
-  return (
-    <View style={styles.evolutionsContainer}>
-      <Text style={styles.sectionTitle}>Evolutions</Text>
-      {evolutions.map((evolution, index) => (
-        <Link key={index} href={`/pages/pokemon-details/${evolution.id}`} style={styles.evolutionItem}>
-          <View>
-            <Text style={styles.evolutionName}>{capitalizeText(evolution.name)}</Text>
-            <Text style={styles.evolutionUrl}>{`https://pokeapi.co/api/v2/pokemon/${evolution.id}/`}</Text>
-          </View>
-        </Link>
-      ))}
-    </View>
-  );
-}
+  const items = evolutions.map((evolution) => ({
+    id: evolution.id,
+    name: evolution.name,
+    url: `https://pokeapi.co/api/v2/pokemon/${evolution.id}/`,
+  }));
 
-const styles = StyleSheet.create({
-  evolutionsContainer: {
-    width: '100%',
-    marginBottom: 16,
-  },
-  sectionTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  evolutionItem: {
-    padding: 10,
-    marginBottom: 5,
-    backgroundColor: ThemeColors.WHITE,
-  },
-  evolutionName: {
-    fontSize: 18,
-    fontWeight: '600',
-  },
-  evolutionUrl: {
-    fontSize: 12,
-    color: ThemeColors.GRAY,
-  },
-});
+  return <ListSection title="Evolutions" items={items} linkPrefix="/pages/pokemon-details/" />;
+}
